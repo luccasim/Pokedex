a3637ffa1598bf96d6e9b85c82ecefd7a9f797b6
Luc Casimir <casimir.luc@gmail.com>
2020-09-12 01:15:38 +0200

    +Gros nettyage de toute facon il y a pas de remote

--- Pokedex/Model/Service/ImageLoader.swift
+++ Pokedex/Model/Service/ImageLoader.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import LCFramework
 
 final class ImageLoader: DataLoader<UIImage> {
     
--- Pokedex/SceneDelegate.swift
+++ Pokedex/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftUI
+import LCFramework
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
